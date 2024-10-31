d18b71a9108b22be5a6a602b0087bced5c4371ff
Md Rashed Hossain <wprashed@icloud.com>
2024-10-31 12:34:33 +0600

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === "complete" && tab.url.includes("amazon.com")) {
+    // Ensure the tab URL exists and is not undefined
+    if (changeInfo.status === "complete" && tab.url && tab.url.includes("amazon.com")) {
       chrome.scripting.executeScript({
         target: { tabId: tabId },
         files: ["content.js"]
